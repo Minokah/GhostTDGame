e7d79f888cd7ede7ddb4c593615ae33574e4cfd1
Henry Chen <chenhenr825@gmail.com>
2025-03-06 16:16:22 -0500

    Basic Game Over Detector(Enemies Reached House)

---
+++ Assets/Scripts/MapTriggers/BadGameOver.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BadGameOver : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collisionObject)
+    {
+        // game over effects here
+    }
+}
