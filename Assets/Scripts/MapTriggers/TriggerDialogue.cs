345abbd1013ad442a81093a0e90d51a98f3c60c6
Henry Chen <chenhenr825@gmail.com>
2025-03-14 10:18:33 -0400

    Added triggerable tutorial dialogue

--- Assets/Scripts/MapTriggers/TriggerDialogue.cs
+++ Assets/Scripts/MapTriggers/TriggerDialogue.cs
@@ -7,6 +7,8 @@ public class TriggerDialogue : MonoBehaviour
 {
     public GameObject dialoguePanel;
     public TMP_Text ghostDialogue;
+    public GameObject infoPanel;
+    public TMP_Text tutorialDialogue;
 
     // this should be triggered by some other object that passes in what level the player is playing, to trigger the right message at the start. EnemySpawner?
     public void initalDialogue(int level)
@@ -15,7 +17,7 @@ public class TriggerDialogue : MonoBehaviour
         {
             dialoguePanel.SetActive(true);
             ghostDialogue.text = "Damm! I shouldn't have knocked over the concealment spell pedestal! Now a horde of humans are going to arrive.\n" +
-                "I need to get some towers up. I can put some down on the open grass.";
+                "I need to call up some spirits. I have some space I can invite them to on the open grass.";
             WaitUtility.Wait(7, () => {
                 dialoguePanel.SetActive(false);
                 ghostDialogue.text = "";
@@ -24,45 +26,89 @@ public class TriggerDialogue : MonoBehaviour
         }
     }
 
-    // should trigger when number of enemies spawned is around half of all enemies that can spawn in the level. Probably trigger these from the enemy spawner.
-    public void middleDialogue(int level)
+    public void tutorialInfo(int level, int dialogueId)
     {
         if (level == 1)
         {
-            dialoguePanel.SetActive(true);
-            ghostDialogue.text = "I'm halfway done my concealment spell. Taking out the rest of the humans shouldn't be too bad...";
-            WaitUtility.Wait(7, () => {
-                dialoguePanel.SetActive(false);
-                ghostDialogue.text = "";
+            if (dialogueId == 1)
+            {
+                infoPanel.SetActive(true);
+                tutorialDialogue.text = "Rude and nasty humans are coming to trash up your house! But don't fear, you can hire spirits who can help!\n " +
+                    "Build appropriate towers for them to inhabit by clicking and placing towers onto the open grass.";
+                WaitUtility.Wait(7, () => {
+                    infoPanel.SetActive(false);
+                    tutorialDialogue.text = "";
+                }
+                );
             }
-            );
+            if (dialogueId == 2)
+            {
+                infoPanel.SetActive(true);
+                tutorialDialogue.text = "These basic humans pose little threat, other than coming in so damm high numbers.\n" +
+                    "The spirit bolts you just hired are good overall defenders, who shoot homing bolts, so they never miss!";
+                WaitUtility.Wait(7, () => {
+                    infoPanel.SetActive(false);
+                    tutorialDialogue.text = "";
+                }
+                );
+            }
+            if (dialogueId == 3)
+            {
+                infoPanel.SetActive(true);
+                tutorialDialogue.text = "Spirits need soul coins to show up and do their job. You can gather more, primarily from reaping the souls of those nasty humans!\n " +
+                    "You get souls for every human you kill.";
+                WaitUtility.Wait(7, () => {
+                    infoPanel.SetActive(false);
+                    tutorialDialogue.text = "";
+                }
+                );
+            }
+            if (dialogueId == 4)
+            {
+                infoPanel.SetActive(true);
+                tutorialDialogue.text = "What kind of ghost doesn't know a few magical tricks? Try out your fire spell on those pesky invaders.\n" +
+                    "Just select from the spell menu, aim, and click on the group of unluck humans.";
+                WaitUtility.Wait(7, () => {
+                    infoPanel.SetActive(false);
+                    tutorialDialogue.text = "";
+                }
+                );
+            }
         }
+    }
+
+    // should trigger when number of enemies spawned is around half of all enemies that can spawn in the level. Probably trigger these from the enemy spawner.
+    public void middleDialogue()
+    {
+        dialoguePanel.SetActive(true);
+        ghostDialogue.text = "I'm halfway done gathering energy for my concealment spell charge. Taking care of the rest of these humans shouldn't be too bad...";
+        WaitUtility.Wait(7, () => {
+            dialoguePanel.SetActive(false);
+            ghostDialogue.text = "";
+        }
+        );
     }
 
     // should trigger when number of enemies spawned is all enemies that can spawn in the level. Probably trigger these from the enemy spawner.
     public void endDialogue(int level)
     {
-        if (level == 1)
-        {
-            dialoguePanel.SetActive(true);
-            ghostDialogue.text = "I'm done my concealment spell! I just got to kill the rest of these humans before I can cast it";
-            WaitUtility.Wait(7, () => {
-                dialoguePanel.SetActive(false);
-                ghostDialogue.text = "";
-            }
-            );
+        dialoguePanel.SetActive(true);
+        ghostDialogue.text = "I'm done gathering energy for my concealment spell charge! I just got to handle the rest of these humans before I can cast it";
+        WaitUtility.Wait(7, () => {
+            dialoguePanel.SetActive(false);
+            ghostDialogue.text = "";
         }
+        );
     }
 
-    // all progress dialogues to inform player when enemy has reached a certain point; We will reuse these detectors for all levels; They retrigger after a while to prevent spamming player with notifications
+    // all progress dialogues to inform player when enemy has reached a certain point; We will reuse these detectors for all levels;
     private void OnTriggerEnter(Collider collisionObject)
     {
         if (collisionObject.gameObject.CompareTag("Enemy") && this.name == "EarlyDetection1")
         {
             this.gameObject.SetActive(false);
             dialoguePanel.SetActive(true);
-            ghostDialogue.text = "Oh damm, those humans are already coming in. They're moving quicker then I expected too. I have to keep them away from my home, until I finish my concealment spell.\n " +
-                "Speaking of spells, maybe I could use some against them right now?";
+            ghostDialogue.text = "Oh damm, those humans are already coming in. They're moving quicker then I expected too. I have to keep them away from my home, until I finish collecting a concealment spell charge.";
             WaitUtility.Wait(7, () => { 
                 dialoguePanel.SetActive(false);
                 //this.gameObject.SetActive(true);
