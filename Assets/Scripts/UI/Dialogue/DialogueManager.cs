dbf226c82ab6ecba36ae8de8b2ec4259b99de749
Henry Chen <chenhenr825@gmail.com>
2025-03-15 19:50:47 -0400

    Dialogue timing bug fix

--- Assets/Scripts/UI/Dialogue/DialogueManager.cs
+++ Assets/Scripts/UI/Dialogue/DialogueManager.cs
@@ -31,26 +31,24 @@ public class DialogueManager : MonoBehaviour
 		if (runDialogue == true){
 			if (Game.EnemySpawner.spawnCount >= (int)(Game.EnemySpawner.maxSpawnCount/2) && midPoint == false){
 				midPoint = true;
-				middleDialogue();
+				StartCoroutine(middleDialogue());
 			}
 			else if (Game.EnemySpawner.spawnCount >= Game.EnemySpawner.maxSpawnCount && finalPoint == false){
 				finalPoint = true;
-				endDialogue();
+				StartCoroutine(endDialogue());
 			}
 			
-			// start displaying tutorial dialogue, with some pauses in between each one
 			StartCoroutine(RunDialogue());
 		}	
 	}
 	
 	public void triggerLevelDialogue(int levelId){
 		level = levelId;
-		initalDialogue();
-		StartCoroutine(InitialWaiter());
+		StartCoroutine(initalDialogue());
 	}
 
     public void resetDialogue(){
-		//StopAllCoroutines();
+		StopAllCoroutines();
 		foreach (GameObject trigger in mapTriggerList){
 			if (trigger != null){
 				trigger.SetActive(true);
@@ -71,110 +69,80 @@ public class DialogueManager : MonoBehaviour
     {
 		runDialogue = false;
 		tutorialInfo();
-        yield return new WaitForSeconds(12);
+        yield return new WaitForSeconds(10);
+		infoPanel.SetActive(false);
 		runDialogue = true;
-    }
-	
-	IEnumerator InitialWaiter()
-    {
-		runDialogue = false;
-        yield return new WaitForSeconds(7);
-		runDialogue = true;
+		dialogueNumber = dialogueNumber + 1;
     }
 	
 	// this should be triggered by some other object that passes in what level the player is playing, to trigger the right message at the start.
-    public void initalDialogue()
+    IEnumerator initalDialogue()
     {
         if (level == 1)
         {
             dialoguePanel.SetActive(true);
             ghostDialogue.text = "Damm! I shouldn't have knocked over the concealment spell pedestal! Now a horde of humans are going to arrive.\n" +
                 "I need to start collecting different energies to get the spell back up!";
-            WaitUtility.Wait(7, () => {
-                dialoguePanel.SetActive(false);
-                ghostDialogue.text = "";
-            }
-            );
+			yield return new WaitForSeconds(10);	
+			dialoguePanel.SetActive(false);
+			runDialogue = true;
+        }
+		
+		if (level == 2)
+        {
+            dialoguePanel.SetActive(true);
+            ghostDialogue.text = "Okay, that's on energy crystal charged. Now I just need to collect 9 more. I might need to look elsewhere for the other 8...";
+			yield return new WaitForSeconds(10);	
+			dialoguePanel.SetActive(false);
+			runDialogue = true;
         }
     }
 	
 	// should trigger when number of enemies spawned is around half of all enemies that can spawn in the level.
-    public void middleDialogue()
+    IEnumerator middleDialogue()
     {
         dialoguePanel.SetActive(true);
         ghostDialogue.text = "I'm halfway done gathering energy for my concealment spell charge. Taking care of the rest of these humans shouldn't be too bad...";
-        WaitUtility.Wait(7, () => {
-            dialoguePanel.SetActive(false);
-            ghostDialogue.text = "";
-        }
-        );
+		yield return new WaitForSeconds(7);
+		dialoguePanel.SetActive(false);
     }
 
     // should trigger when number of enemies spawned is all enemies that can spawn in the level.
-    public void endDialogue()
+    IEnumerator endDialogue()
     {
         dialoguePanel.SetActive(true);
         ghostDialogue.text = "I'm done gathering energy for my concealment spell charge! I just got to handle the rest of these humans before I can cast it";
-        WaitUtility.Wait(7, () => {
-            dialoguePanel.SetActive(false);
-            ghostDialogue.text = "";
-        }
-        );
+		yield return new WaitForSeconds(7);
+		dialoguePanel.SetActive(false);
     }
 
     public void tutorialInfo()
     {
         if (level == 1)
         {
-			Debug.Log("level: " + level);
-			Debug.Log("dialogueNumber: " + dialogueNumber);
             if (dialogueNumber == 0)
             {
                 infoPanel.SetActive(true);
                 tutorialDialogue.text = "Rude and nasty humans are coming to trash up your house! But don't fear, you can hire spirits who can help!\n " +
                     "Build appropriate towers for them to inhabit by clicking and placing towers onto the open grass.";
-                WaitUtility.Wait(7, () => {
-                    infoPanel.SetActive(false);
-                    tutorialDialogue.text = "";
-					dialogueNumber = dialogueNumber + 1;
-                }
-                );
             }
             if (dialogueNumber == 1)
             {
                 infoPanel.SetActive(true);
                 tutorialDialogue.text = "These basic humans pose little threat, other than coming in so damm high numbers.\n" +
                     "The spirit bolts you just hired are good overall defenders, who shoot homing bolts, so they never miss!";
-                WaitUtility.Wait(7, () => {
-                    infoPanel.SetActive(false);
-                    tutorialDialogue.text = "";
-					dialogueNumber = dialogueNumber + 1;
-                }
-                );
             }
             if (dialogueNumber == 2)
             {
                 infoPanel.SetActive(true);
                 tutorialDialogue.text = "Spirits need soul coins to show up and do their job. You can gather more, primarily from reaping the souls of those nasty humans!\n " +
                     "You get souls for every human you kill.";
-                WaitUtility.Wait(7, () => {
-                    infoPanel.SetActive(false);
-                    tutorialDialogue.text = "";
-					dialogueNumber = dialogueNumber + 1;
-                }
-                );
             }
             if (dialogueNumber == 3)
             {
                 infoPanel.SetActive(true);
                 tutorialDialogue.text = "What kind of ghost doesn't know a few magical tricks? Try out your fire spell on those pesky invaders.\n" +
                     "Just select from the spell menu, aim, and click on the group of unlucky humans.";
-                WaitUtility.Wait(7, () => {
-                    infoPanel.SetActive(false);
-                    tutorialDialogue.text = "";
-					dialogueNumber = dialogueNumber + 1;
-                }
-                );
             }
         }
     }
