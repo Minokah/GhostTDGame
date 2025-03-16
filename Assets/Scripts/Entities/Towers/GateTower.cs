5230d58f4f9f8efcd81c4cae1705b09ba86780ff
Henry Chen <chenhenr825@gmail.com>
2025-03-16 18:46:26 -0400

    Gate Tower rebalancing

--- Assets/Scripts/Entities/Towers/GateTower.cs
+++ Assets/Scripts/Entities/Towers/GateTower.cs
@@ -53,8 +53,8 @@ public class GateTower : Entity
     public void generalPatrol()
     {
         spawner = GameObject.FindWithTag("Enemy Spawner");
-        spawner.GetComponent<EnemySpawner>().reduceSpawnEffect(2);
-        spawner.GetComponent<EnemySpawner>().longerBreakEffect(0.375f);
+        spawner.GetComponent<EnemySpawner>().reduceSpawnEffect(5);
+        spawner.GetComponent<EnemySpawner>().longerBreakEffect(0.35f);
     }
 
     public void specialPatrol()
