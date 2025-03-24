7b8fb309994d27af02a2e51eb0ab2d410cbcdb44
Henry Chen <chenhenr825@gmail.com>
2025-03-23 21:29:39 -0400

    Remove unused imports and test scences

--- Assets/Scripts/UI/Progression Menu/ProgressionMenuUpgrades.cs
+++ Assets/Scripts/UI/Progression Menu/ProgressionMenuUpgrades.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 using static UnityEngine.EventSystems.EventTrigger;
 
 public class ProgressionMenuUpgrades : MonoBehaviour
